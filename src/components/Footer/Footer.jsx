e1426fc5fff6f6427ce358853d2e969eaf259e53
Masudur400 <md8004210@gmail.com>
2024-06-05 23:01:38 +0600

    last update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,29 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoLogoTwitter, IoLogoYoutube } from "react-icons/io";
 
 const Footer = () => {
     return (
         <div className="border-t-2 border-yellow-600">
             <footer className="footer p-10 container  mx-auto">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
+                    {/* <h6 className="footer-title">Services</h6>
                     <a className="link link-hover">Branding</a>
                     <a className="link link-hover">Design</a>
                     <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <a className="link link-hover">Advertisement</a> */}
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <h6 className="footer-title">Social</h6>
+                    <div className="grid grid-flow-col gap-4">
+                        <a className="text-3xl"> <IoLogoTwitter></IoLogoTwitter></a>
+                        <a className="text-3xl"> <FaFacebookSquare></FaFacebookSquare></a>
+                        <a className="text-3xl"> <IoLogoYoutube></IoLogoYoutube></a>
+                    </div>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <h6 className="footer-title">Contact Us</h6>
+                    <p className="font-bold">Email : md8004210@gmail.com</p>
+                    <p className="font-bold">WhatsApp : 01905711766</p> 
                 </nav>
             </footer>
         </div>
